83d5de81aeaef6bf2192a1eec2a98efffa535502
Jason <jasonmy.liang@mail.utoronto.ca>
2022-09-22 21:07:01 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,71 @@
+
+public class Polynomial
+{
+    public double[] array;
+    public Polynomial() {
+
+        array = new double[]{0};
+    }
+    public Polynomial(double[] x){
+
+        array = x;
+    }
+    public Polynomial add(Polynomial x)
+    {
+        if (x.array.length == 0) //Zero Cases
+        {
+            return this;
+        }
+        if (array.length == 0)
+        {
+            return x;
+        }
+        if (array.length >= x.array.length) // Non-Cases
+        {
+            for (int i = 0; i<x.array.length;i++)
+            {
+                array[i] = x.array[i]+array[i];
+            }
+            return this;
+        }
+        else
+        {
+            for (int i = 0; i<array.length;i++)
+            {
+                x.array[i] = x.array[i]+array[i];
+            }
+            return x;
+        }
+    }
+    public double evaluate(double x){
+        if (array.length != 0) //non-zero array case
+        {
+            double num = 0;
+            double power = 0;
+            for (int i = 0 ; i < array.length ; i++)
+            {
+                num = num + array[i]*Math.pow(x,power);
+                power = power + 1;
+            }
+            return num;
+        }
+        return 0.0;
+    }
+    public Boolean hasRoot(double x)
+    {
+        if (array.length != 0) //non-zero array case
+        {
+            if (evaluate(x) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+}
